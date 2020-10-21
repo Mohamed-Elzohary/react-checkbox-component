122af4c0826dcdfae3d0b08d57afb1fd7c9ac420
Mohamed-Elzohary <48733136+Mohamed-Elzohary@users.noreply.github.com>
2020-10-21 10:21:31 +0200

    added: Layout Sidebar + MainContent

--- src/App.js
+++ src/App.js
@@ -1,77 +1,13 @@
 import React, { Component } from "react";
 import "./styles.css";
 
-import Checkbox from "./components/Checkbox/Checkbox.comp";
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
-  constructor(prop) {
-    super();
-    this.state = {
-      lectures: [
-        {
-          id: 1,
-          title: "الدرس الأول"
-        },
-        {
-          id: 2,
-          title: "الدرس الثاني"
-        },
-        {
-          id: 3,
-          title: "الدرس الثالث"
-        },
-        {
-          id: 4,
-          title: "الدرس الرابع"
-        }
-      ],
-      total_lecs: 0,
-      finished_lecs: 0
-    };
-  }
-
-  componentDidMount() {
-    if (this.state.lectures.length > 0) {
-      this.setState({
-        total_lecs: this.state.lectures.length
-      });
-    } else {
-      this.setState({
-        total_lecs: 0
-      });
-    }
-  }
-
-  cbxChange = (e) => {
-    console.log("cbx changed: " + e.target.id + " - " + e.target.checked);
-
-    if (e.target.checked) {
-      this.setState({ finished_lecs: this.state.finished_lecs + 1 });
-    } else {
-      this.setState({ finished_lecs: this.state.finished_lecs - 1 });
-    }
-  };
-
   render() {
     return (
       <div className="App">
-        <div className="lectures">
-          <h3>إذا أنهيت درسك فاضغط على علامة الصح</h3>
-          <h3>
-            {this.state.finished_lecs} / {this.state.total_lecs}
-          </h3>
-
-          {this.state.lectures.map((lecture) => {
-            return (
-              <Checkbox
-                key={lecture.id}
-                id={lecture.id}
-                title={lecture.title}
-                onChange={this.cbxChange}
-              />
-            );
-          })}
-        </div>
+        <Layout />
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Layout.css";
+
+import MainContent from "../MainContent/MainContent";
+import Sidebar from "../Sidebar/Sidebar";
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  toggleSidebar() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <Sidebar
+          toggleSidebar={this.toggleSidebar.bind(this)}
+          expanded={this.state.expanded}
+        />
+        <MainContent expanded={this.state.expanded} />
+      </main>
+    );
+  }
+}
+
+export default Layout;
